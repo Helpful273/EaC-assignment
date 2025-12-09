7f8d015cb3542018684712f4e6af537ca135e3ea
uarecoma@gmail.com <uarecoma@gmail.com>
2025-12-09 13:40:33 -0500

    fixes and java doc

--- src/Objects/Option.java
+++ src/Objects/Option.java
@@ -1,43 +1,63 @@
 package Objects;
 
+import java.util.HashMap;
+
 /**
  *
- * @author Helpful
+ * @author helpful
  */
 public class Option {
+    private HashMap<String, OptionEntry> entries = new HashMap<>();
+    private OptionEntry selectedEntry;
     private String name;
-    private double uxFactor, privacyFactor;
-    private String uxOperation, privacyOperation;
     
     /*
-    Constructor for ScoreImpact object.
-    @param uxFactor The factor in which it affects the calculation of user experience score.
-    @param privacyFactor The factor in which it affects the calculation of privacy score.
-    @param uxOperation The operation to calculate the user experience score with.
-    @param privacyOperation The operation to calculate the privacy score with.
+    Constructor for a SettingEntry object.
+    @param name The setting's name.
+    @param entries The varargs for default entries.
     */
-    public Option(String name, double uxFactor, double privacyFactor, String uxOperation, String privacyOperation) {
+    public Option(String name, OptionEntry... entries) {
         this.name = name;
-        this.uxFactor = uxFactor;
-        this.privacyFactor = privacyFactor;
-        this.uxOperation = uxOperation;
-        this.privacyOperation = privacyOperation;
+        this.selectedEntry = entries[0];
+        
+        for (OptionEntry entry: entries) {
+            this.entries.put(entry.GetName(), entry);
+        }
     }
     
     /*
-    Calculates the user experience score based off a preexisting
+    Adds another OptionEntry to the options hashmap.
+    @param entry The OptionEntry object to add.
     */
-    public double CalculateUXScore() {
-        double finalUXScore = 0;
-        return finalUXScore;
+    public void AddEntry(OptionEntry entry) {
+        this.entries.put(entry.GetName(), entry);
     }
     
-    public double CalculatePrivacyScore() {
-        double finalPrivacyScore = 0;
-        return finalPrivacyScore;
+    /*
+    Selects an OptionEntry object based on their defined name.
+    @param entryName The name to look for.
+    */
+    public void SelectEntry(String entryName) {
+        if (this.entries.containsKey(entryName)) {
+            this.selectedEntry = this.entries.get(entryName);
+        } else {
+            System.out.println(String.format("Key: \"%s\" does not exist within options. Option remains the same.", entryName));
+        }
     }
     
-    public String GetOptionName() {
+    /*
+    Fetch our current OptionEntry object
+    @return OptionEntry
+    */
+    public OptionEntry GetSelectedEntry() {
+        return this.selectedEntry;
+    }
+    
+    /*
+    Fetch our Option object name
+    @return String The name of our option
+    */
+    public String GetName() {
         return this.name;
     }
 }
---
+++ src/Objects/OptionEntry.java
@@ -0,0 +1,43 @@
+package Objects;
+
+/**
+ *
+ * @author Helpful
+ */
+public class OptionEntry {
+    private String name;
+    private double uxFactor, privacyFactor;
+    private String uxOperation, privacyOperation;
+    
+    /*
+    Constructor for ScoreImpact object.
+    @param uxFactor The factor in which it affects the calculation of user experience score.
+    @param privacyFactor The factor in which it affects the calculation of privacy score.
+    @param uxOperation The operation to calculate the user experience score with.
+    @param privacyOperation The operation to calculate the privacy score with.
+    */
+    public OptionEntry(String name, double uxFactor, double privacyFactor, String uxOperation, String privacyOperation) {
+        this.name = name;
+        this.uxFactor = uxFactor;
+        this.privacyFactor = privacyFactor;
+        this.uxOperation = uxOperation;
+        this.privacyOperation = privacyOperation;
+    }
+    
+    /*
+    Calculates a new user experience score given 
+    */
+    public double CalculateUXScore() {
+        double finalUXScore = 0;
+        return finalUXScore;
+    }
+    
+    public double CalculatePrivacyScore() {
+        double finalPrivacyScore = 0;
+        return finalPrivacyScore;
+    }
+    
+    public String GetName() {
+        return this.name;
+    }
+}
